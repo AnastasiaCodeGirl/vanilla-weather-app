4d018bf260cbee61b7fc1c6f489624a73482bcfa
Anastasiia Shumeko <vyahirevan@gmail.com>
2023-05-27 18:38:50 +0200

    Added HTML structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-alert("Hello World");
+function displayTemperature(response) {
+	console.log(response.data.main.temp);
+}
+let apiKey = "917b5cb46b9991bd0ab660f50601d0c6";
+let cityName = "Malaga";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
