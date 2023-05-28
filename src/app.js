40535ab5856c7dabed34211def69805aa4a265e2
Anastasiia Shumeko <vyahirevan@gmail.com>
2023-05-28 11:20:41 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 function displayTemperature(response) {
-	console.log(response.data.main.temp);
+	let temperatureElement = document.querySelector("#temperature");
+	temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.city;
+	let descriptionElement = document.querySelector("#description");
+	descriptionElement.innerHTML = response.data.condition.description;
+	let humidityElement = document.querySelector("#humidity");
+	humidityElement.innerHTML = response.data.temperature.humidity;
+	let windElement = document.querySelector("#wind");
+	windElement.innerHTML = Math.round(response.data.wind.speed);
 }
-let apiKey = "917b5cb46b9991bd0ab660f50601d0c6";
-let cityName = "Malaga";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+let apiKey = "fed24a4a3934t32fo5a63bbe36a70167";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Amsterdam&key=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
